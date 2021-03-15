0ced7d668739e9ba97c5bc0654038c856344f817
sadek666 <you@example.com>
2021-03-15 14:02:56 +0100

    User entity, User dto, User validation

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,23 @@
-export class CreateUserDto {}
+import { IsDateString, IsString, MaxLength, MinLength } from 'class-validator';
+const minDate = new Date()
+minDate.setFullYear(1900, 0, 1)
+
+export class CreateUserDto {
+  @IsString()
+  @MinLength(2)
+  @MaxLength(255)
+  name: string;
+
+  @IsString()
+  @MinLength(2)
+  @MaxLength(255)
+  surname: string;
+
+  @IsDateString()
+  birthDate: string;
+
+  @IsString()
+  @MinLength(2)
+  @MaxLength(255)
+  nationality: string;
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,25 @@
-export class User {}
+import { Column, CreateDateColumn, PrimaryGeneratedColumn } from 'typeorm';
+
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: false, type: 'varchar', length: 255 })
+  name: string;
+
+  @Column({ nullable: false, type: 'varchar', length: 255 })
+  surname: string;
+
+  @Column({ nullable: false, type: 'date', length: 255, name: 'birth_date' })
+  birthDate: string;
+
+  @Column({ nullable: false, type: 'varchar', length: 255 })
+  nationality: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  // @ManyToMany(() => ItemEntity)
+  // @JoinTable()
+  // item: ItemEntity[];
+}
